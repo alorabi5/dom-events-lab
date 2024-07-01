23b17790afb579c2dac3913cb5be4fbe90dd1771
Redha Alaraibi <reda9808@gmail.com>
2024-07-01 03:31:17 +0300

    HW calculator  submit

---
+++ app.js
@@ -0,0 +1,75 @@
+/*-------------------------------- Constants --------------------------------*/
+const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+const display = document.querySelector('.display');
+
+/*-------------------------------- Variables --------------------------------*/
+let opResult, num1, num2, opType;
+/*------------------------ Cached Element References ------------------------*/
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+function init(){
+    opResult = 0;
+    num1 = 0;
+    num2 = 0;
+    opType = '';
+    display.textContent = '0'
+}
+
+function resutl(num1, num2, opType){
+    switch(opType){
+        case '/':
+            return num1 / num2;
+        case '*':
+            return num1 * num2;
+        case '+':
+            return Number(num1) + Number(num2);
+        case '-':
+            return num1 - num2;
+        default:
+            return;
+    }
+}
+
+init();
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+calculator.addEventListener('click', (event) => {
+    // This log is for testing purposes to verify we're getting the correct value
+    // You have to click a button to see this log
+    // console.log(event.target.innerText);
+  
+    // Example
+    if (event.target.classList.contains('number')) {
+        
+        if(!opType){
+            
+            num1 += event.target.innerText;
+            display.textContent = `${num1}`;
+            
+        }
+        else{
+            num2 += event.target.innerText;
+            display.textContent = `${num1} ${opType} ${num2}`;
+            
+        }
+    }
+  
+    // Example
+    else if (event.target.innerText === 'C') {
+      // Do something with this operator
+        init();
+    }
+    else if (event.target.innerText === '=') {
+        // Do something with this operator
+        opResult = resutl(num1, num2, opType);
+        display.textContent = ` ${opResult} `;
+    }
+    else{
+        opType = event.target.innerText;
+        display.textContent = `${num1} ${opType} `;
+    }
+  });
